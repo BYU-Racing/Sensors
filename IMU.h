95f7ee4964ff83a172b38c33ff16df231d331c76
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-04 00:20:08 -0600

    Impl debugPrint

--- IMU.h
+++ IMU.h
@@ -12,11 +12,13 @@ class IMU final : public Sensor
     BNO08x_RVC_Data* heading = nullptr;
     static void pack(uint8_t* buf, float value, uint8_t id);
     static float unpack(const uint8_t* buf);
+    static void printValue(const char label[], float value, const char units[]);
 public:
     IMU(uint32_t id, bool criticality, uint32_t readInterval, Adafruit_BNO08x_RVC* rvc);
     ~IMU() override;
     bool ready() override;
     SensorData read() override;
+    void debugPrint(const CAN_message_t& canMsg) const override;
 };
 
 #endif //IMU_H
