e049ef9a393c2e0850f758ecad9cfa0db831134a
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-04 00:15:08 -0600

    Fix for wrong float handling

--- ByteConverter.cpp
+++ ByteConverter.cpp
@@ -1,41 +1,61 @@
 #include "ByteConverter.h"
 
-float ByteConverter::toFloat(const uint8_t* bytes)
+/**
+ * Bit unshifts and combines all bytes from Big-Endian format
+ * @param bytes array with 4 Big-Endian bytes
+ * @return
+ */
+uint32_t ByteConverter::uint32FromBytes(const uint8_t* bytes)
 {
-    float value;
-    uint8_t* valueBytes = reinterpret_cast<uint8_t*>(&value);
-    for (size_t i = 0; i < sizeof(float); i++)
-    {
-        valueBytes[i] = bytes[i];
-    }
-    return value;
+    return bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
 }
-
-void ByteConverter::toBytes(float value, uint8_t* buf)
+/**
+ * Bit shifts and masks each byte then stores in Big-Endian format
+ * @param value value to convert
+ * @param buf array to store 4 Big-Endian bytes
+ */
+void ByteConverter::uint32ToBytes(const uint32_t value, uint8_t* buf)
 {
-    const uint8_t* valueBytes = reinterpret_cast<uint8_t*>(&value);
-    for (size_t i = 0; i < sizeof(float); i++)
-    {
-        buf[i] = valueBytes[i];
-    }
+    buf[0] = value >> 24 & 0xFF;
+    buf[1] = value >> 16 & 0xFF;
+    buf[2] = value >> 8 & 0xFF;
+    buf[3] = value & 0xFF;
 }
-
-int ByteConverter::toInt(const uint8_t* bytes)
+/**
+ * Extract uint32_t from bytes then reinterpret as float
+ * @param bytes array with 4 Big-Endian bytes
+ * @return
+ */
+float ByteConverter::floatFromBytes(const uint8_t* bytes)
 {
-    int value;
-    uint8_t* valueBytes = reinterpret_cast<uint8_t*>(&value);
-    for (size_t i = 0; i < sizeof(int); i++)
-    {
-        valueBytes[i] = bytes[i];
-    }
-    return value;
+    uint32_t intValue = uint32FromBytes(bytes);
+    return *reinterpret_cast<float*>(&intValue);
 }
-
-void ByteConverter::toBytes(int value, uint8_t* buf)
+/**
+ * Reinterprets as uint32_t then stores bytes in buf
+ * @param value float to convert
+ * @param buf array to store 4 Big-Endian bytes
+ */
+void ByteConverter::floatToBytes(const float value, uint8_t* buf)
 {
-    const uint8_t* valueBytes = reinterpret_cast<uint8_t*>(&value);
-    for (size_t i = 0; i < sizeof(int); i++)
-    {
-        buf[i] = valueBytes[i];
-    }
+    const uint32_t intValue = *reinterpret_cast<const uint32_t*>(&value);
+    uint32ToBytes(intValue, buf);
+}
+/**
+ * Extracts uint32_t from bytes then casts to int
+ * @param bytes array with 4 Big-Endian bytes
+ * @return
+ */
+int ByteConverter::intFromBytes(const uint8_t* bytes)
+{
+    return static_cast<int>(uint32FromBytes(bytes));
+}
+/**
+ * Casts to int then stores bytes in buf
+ * @param value
+ * @param buf
+ */
+void ByteConverter::intToBytes(const int value, uint8_t* buf)
+{
+    uint32ToBytes(static_cast<uint32_t>(value), buf);
 }
