25466c0bf485ba2308beaf493929dbad291131e7
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-05 17:49:50 -0600

    Added Switch subclass with better debugPrint

--- DigitalSensor.h
+++ DigitalSensor.h
@@ -4,12 +4,20 @@
 #include <Arduino.h>
 #include "Sensor.h"
 
-class DigitalSensor final : public Sensor
+class DigitalSensor : public Sensor
 {
     uint8_t pin = 0;
 public:
     DigitalSensor(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval);
     SensorData read() override;
+};
+
+class Switch : public DigitalSensor
+{
+    const char* name;
+public:
+    Switch(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval, const char* name);
+    void debugPrint(const CAN_message_t& canMsg) const override;
 };
 
 #endif //DIGITALSENSOR_H
