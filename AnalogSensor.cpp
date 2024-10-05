d5a8765bd1f89b613e09ed8b055c8a907788ef78
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-05 17:50:57 -0600

    Added Potentiometer subclass with better debugPrint

--- AnalogSensor.cpp
+++ AnalogSensor.cpp
@@ -20,3 +20,19 @@ SensorData AnalogSensor::read()
 
     return sensorData;
 }
+
+Pot::Pot(
+    const uint32_t id, const bool criticality, const uint8_t pin, const uint32_t readInterval, const char* name
+) : AnalogSensor(id, criticality, pin, readInterval), name(name) {  }
+
+void Pot::debugPrint(const CAN_message_t& canMsg) const
+{
+    Serial.print(name);
+    Serial.println(" Potentiometer CAN Message:");
+    Serial.print("Timestamp: ");
+    Serial.println(canMsg.timestamp);
+    const int value = ByteConverter::intFromBytes(canMsg.buf);
+    Serial.print("Value: ");
+    Serial.println(value);
+    Serial.println();
+}
