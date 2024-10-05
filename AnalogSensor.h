c65af4ca13c2815e8738cfb6a0f60a49bca02bef
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-05 17:51:26 -0600

    Added Potentiometer subclass with better debugPrint

--- AnalogSensor.h
+++ AnalogSensor.h
@@ -4,12 +4,20 @@
 #include <Arduino.h>
 #include "Sensor.h"
 
-class AnalogSensor final : public Sensor
+class AnalogSensor : public Sensor
 {
     uint8_t pin = 0;
 public:
     AnalogSensor(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval);
     SensorData read() override;
+};
+
+class Pot : public AnalogSensor
+{
+    const char* name;
+public:
+    Pot(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval, const char* name);
+    void debugPrint(const CAN_message_t& canMsg) const override;
 };
 
 #endif //ANALOGSENSOR_H
