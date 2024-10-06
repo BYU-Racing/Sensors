6b1ef8fe7472b9350dfbe4fe0cdb3b9635886e0a
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-06 13:03:53 -0600

    Implemented healthCheck - removed Switch

--- DigitalSensor.h
+++ DigitalSensor.h
@@ -9,15 +9,8 @@ class DigitalSensor : public Sensor
     uint8_t pin = 0;
 public:
     DigitalSensor(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval);
+    [[nodiscard]] bool healthCheck() const override;
     SensorData read() override;
-};
-
-class Switch : public DigitalSensor
-{
-    const char* name;
-public:
-    Switch(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval, const char* name);
-    void debugPrint(const CAN_message_t& canMsg) const override;
 };
 
 #endif //DIGITALSENSOR_H
