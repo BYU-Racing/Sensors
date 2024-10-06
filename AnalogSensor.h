468464a4ce98c759fde0f0736f742a8142f65405
Braden Toone <103770034+Braden2n@users.noreply.github.com>
2024-10-06 13:01:01 -0600

    Implemented healthCheck - removed Pot

--- AnalogSensor.h
+++ AnalogSensor.h
@@ -9,15 +9,8 @@ class AnalogSensor : public Sensor
     uint8_t pin = 0;
 public:
     AnalogSensor(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval);
+    [[nodiscard]] bool healthCheck() const override;
     SensorData read() override;
-};
-
-class Pot : public AnalogSensor
-{
-    const char* name;
-public:
-    Pot(uint32_t id, bool criticality, uint8_t pin, uint32_t readInterval, const char* name);
-    void debugPrint(const CAN_message_t& canMsg) const override;
 };
 
 #endif //ANALOGSENSOR_H
