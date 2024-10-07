0d3d3eca4629d7697a44ab4ab4cabd7dd358a57e
btoone <braden@toonetown.com>
2024-10-07 02:21:02 -0600

    Added RotationSensor to DataCollection example

--- examples/DataCollection.cpp
+++ examples/DataCollection.cpp
@@ -1,32 +1,43 @@
 #include <Arduino.h>
 #include <FlexCAN_T4.h>
+#include <Adafruit_BNO08x_RVC.h>
 #include "AnalogSensor.h"
 #include "DigitalSensor.h"
+#include "RVC.h"
+#include "RotationSensor.h"
 
-constexpr uint32_t R_DAMPER_ID = 12;
+constexpr uint32_t R_DAMPER_ID = 1;
 constexpr bool R_DAMPER_CRITICALITY = false;
 constexpr uint8_t R_DAMPER_PIN = 22;
 
-constexpr uint32_t L_DAMPER_ID = 13;
+constexpr uint32_t L_DAMPER_ID = 2;
 constexpr bool L_DAMPER_CRITICALITY = false;
 constexpr uint8_t L_DAMPER_PIN = 23;
 
 constexpr uint32_t DAMPER_INTERVAL = 100;
 
-constexpr uint32_t SWITCH_ID = 15;
+constexpr uint32_t SWITCH_ID = 3;
 constexpr bool SWITCH_CRITICALITY = true;
 constexpr uint8_t SWITCH_PIN = 38;
 constexpr uint32_t SWITCH_INTERVAL = 100;
 
-constexpr uint32_t RVC_ID = 1;
+constexpr uint32_t RVC_ID = 4;
 constexpr bool RVC_CRITICALITY = false;
 constexpr uint32_t RVC_INTERVAL = 100;
+
+constexpr uint32_t STEERING_WHEEL_ID = 5;
+constexpr bool STEERING_WHEEL_CRITICALITY = false;
+constexpr uint32_t STEERING_WHEEL_INTERVAL = 100;
 
 AnalogSensor rightDamperPot = AnalogSensor(R_DAMPER_ID, R_DAMPER_CRITICALITY, R_DAMPER_PIN, DAMPER_INTERVAL);
 AnalogSensor leftDamperPot = AnalogSensor(L_DAMPER_ID, L_DAMPER_CRITICALITY, L_DAMPER_PIN, DAMPER_INTERVAL);
 DigitalSensor startSwitch = DigitalSensor(SWITCH_ID, SWITCH_CRITICALITY, SWITCH_PIN, SWITCH_INTERVAL);
-Adafruit_BNO08x_RVC bno;
-RVC rvc = RVC(RVC_ID, RVC_CRITICALITY, RVC_INTERVAL, &bno);
+Adafruit_BNO08x_RVC bno1;
+RVC rvc = RVC(RVC_ID, RVC_CRITICALITY, RVC_INTERVAL, &bno1);
+Adafruit_BNO08x bno2;
+RotationSensor steeringWheel = RotationSensor(
+    STEERING_WHEEL_ID, STEERING_WHEEL_CRITICALITY, STEERING_WHEEL_INTERVAL, &bno2
+);
 
 Sensor* sensors[] = {
     &rightDamperPot,
@@ -39,21 +50,21 @@ size_t numSensors = sizeof(sensors) / sizeof(sensors[0]);
 void setup() {
     Serial.begin(115200);
     Serial1.begin(115200);
-    rvc.begin(&Serial1); // Some sensors have a .begin() method that must be called for proper operation
+    rvc.begin(&Serial1);
+    steeringWheel.begin(&Serial1);
 }
 
 void loop() {
-    for (size_t i = 0; i < numSensors; i++)
+    for (size_t sensorIndex = 0; sensorIndex < numSensors; sensorIndex++)
     {
-        Sensor* sensor = sensors[i];
-        if (sensor->healthCheck() && sensor->ready())
+        if (Sensor* sensor = sensors[sensorIndex]; sensor->healthCheck() && sensor->ready())
         {
             const SensorData data = sensor->read();
             const size_t msgCount = data.getMsgCount();
             const CAN_message_t* msgs = data.getMsgs();
-            for (size_t i = 0; i < msgCount; i++)
+            for (size_t msgIndex = 0; msgIndex < msgCount; msgIndex++)
             {
-                sensor->debugPrint(msgs[i]);
+                sensor->debugPrint(msgs[msgIndex]);
             }
         }
     }
